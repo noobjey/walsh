93f4f6964e44ae9260a03a6043e35a8233b76ced
noobjey <noobjey@gmail.com>
2014-08-03 19:36:54 -0500

    initial commit after yoing, added one page before remebering to do intial commit

---
+++ app/scripts/controllers/home.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name walshApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the walshApp
+ */
+angular.module('walshApp')
+  .controller('HomeCtrl', function ($scope) {
+  	console.log('in home ctrl.');
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name walshApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the walshApp
+ */
+angular.module('walshApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
